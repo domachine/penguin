3e9d6c26e43058a4931c75946132c471ec436060
Dominik Burgdörfer <db@domachine.de>
2017-02-01 09:56:57 +0100

    Use create-component-map tool using javascript api

--- bin/pack.js
+++ bin/pack.js
@@ -6,7 +6,6 @@ const crypto = require('crypto')
 const { Writable } = require('stream')
 const { dirname, join, basename, extname } = require('path')
 const fs = require('fs')
-const { spawnSync } = require('child_process')
 const { mkdir, test } = require('shelljs')
 const glob = require('glob')
 const mkdirp = require('mkdirp')
@@ -18,6 +17,7 @@ const render404HTML = require('../pages/404')
 const buildClientRuntime = require('./build_client_runtime')
 const buildServerRuntime = require('./build_server_runtime')
 const compileTemplate = require('./compile_template')
+const createComponentMap = require('./create_component_map')
 
 const mkdirpAsync = Bluebird.promisify(mkdirp)
 
@@ -44,53 +44,56 @@ function pack ({ viewEngine = 'dust', languages }) {
   if (!test('-f', 'files/404.html')) {
     fs.writeFileSync('files/404.html', render404HTML({ languages }))
   }
-  const env = Object.assign({}, process.env, { NODE_ENV: 'production' })
-  spawnSync(`${__dirname}/create_component_map.js`, [
-    'components', '-b', '-o', '.penguin/components.js'
-  ], { stdio: 'inherit', env })
-  spawnSync(`${__dirname}/create_component_map.js`, [
-    'components', '-o', '.penguin/server_components.js'
-  ], { stdio: 'inherit', env })
   const files = glob.sync('@(objects|pages)/*.' + viewEngine)
-  return files.reduce((p, file) =>
-    p.then(files =>
-      new Promise((resolve, reject) => {
-        console.error('build server runtime for %s', file)
-        buildServerRuntime({ file })
-          .on('error', reject)
-          .pipe(fs.createWriteStream(file.replace(/\.[^.]+$/, '.js')))
-          .on('error', reject)
-          .on('finish', () => resolve())
-      })
-      .then(() => {
-        let buffer = ''
-        const hash = crypto.createHash('md5')
-        return new Promise((resolve, reject) => {
-          console.error('build client runtime for %s', file)
-          buildClientRuntime({ file })
+  return Promise.all([
+    createComponentMap({
+      browser: true,
+      pattern: 'components/*',
+      output: '.penguin/components.js'
+    }),
+    createComponentMap({ output: '.penguin/server_components.js' })
+  ])
+  .then(() =>
+    files.reduce((p, file) =>
+      p.then(files =>
+        new Promise((resolve, reject) => {
+          console.error('build server runtime for %s', file)
+          buildServerRuntime({ file })
             .on('error', reject)
-            .pipe(new Writable({
-              write (chunk, enc, callback) {
-                hash.update(chunk)
-                buffer += chunk
-                callback()
-              }
-            }))
-            .on('finish', () => {
-              const path =
-                join(
-                  'static',
-                  file.replace(/\.[^.]+$/, `.${hash.digest('hex')}.js`)
-                )
-              mkdir('-p', dirname(path))
-              fs.writeFile(path, buffer, err =>
-                err ? reject(err) : resolve([...files, '/' + path])
-              )
-            })
+            .pipe(fs.createWriteStream(file.replace(/\.[^.]+$/, '.js')))
+            .on('error', reject)
+            .on('finish', () => resolve())
         })
-      })
-    )
-  , Promise.resolve([]))
+        .then(() => {
+          let buffer = ''
+          const hash = crypto.createHash('md5')
+          return new Promise((resolve, reject) => {
+            console.error('build client runtime for %s', file)
+            buildClientRuntime({ file })
+              .on('error', reject)
+              .pipe(new Writable({
+                write (chunk, enc, callback) {
+                  hash.update(chunk)
+                  buffer += chunk
+                  callback()
+                }
+              }))
+              .on('finish', () => {
+                const path =
+                  join(
+                    'static',
+                    file.replace(/\.[^.]+$/, `.${hash.digest('hex')}.js`)
+                  )
+                mkdir('-p', dirname(path))
+                fs.writeFile(path, buffer, err =>
+                  err ? reject(err) : resolve([...files, '/' + path])
+                )
+              })
+          })
+        })
+      )
+    , Promise.resolve([]))
+  )
   .then((filesRuntimes) =>
     Promise.all(
       files.map((file, i) => {
