28ec372820c15cb80ae542c7518580d12687f79c
Dominik Burgdörfer <db@domachine.de>
2017-02-24 15:56:33 +0100

    Add publish actions

--- src/actions.js
+++ src/actions.js
@@ -3,6 +3,9 @@ export const UPDATE_FIELDS = 'UPDATE_FIELDS'
 export const SAVE = 'SAVE'
 export const SAVE_SUCCESS = 'SAVE_SUCCESS'
 export const SAVE_FAILURE = 'SAVE_FAILURE'
+export const PUBLISH = 'PUBLISH'
+export const PUBLISH_SUCCESS = 'PUBLISH_SUCCESS'
+export const PUBLISH_FAILURE = 'PUBLISH_FAILURE'
 
 export function update (update) {
   return { type: UPDATE_FIELDS, update }
